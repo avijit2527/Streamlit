3cf62de85f9e7e46fd27ae7e29466e602749158d
Avijit Roy <avijit.roy@scaler.com>
2024-08-26 22:58:13 +0530

    Built Streamlit App

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.header("Learning is fun!!!! ")
+
+
+code = '''def hello():
+    print("Hello, Streamlit!")'''
+st.code(code, language="python")
+
+st.subheader("_Streamlit_ is :blue[cool] :sunglasses:")
